0d0e36adf0db4b74d66aab1b194d30c2d1c1af8b
podlesniyag <podlesniyag@yandex.ru>
2015-07-04 12:53:49 +0300

    create calc

--- calculate.java
+++ calculate.java
@@ -3,5 +3,9 @@ public class calculate
    public static void main(String[] arg)
 	{
 		System.out.println("calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		System.out.println("Summ = " + summ);
 	};  
 };
