2cec8e7a4dc8d50499804263eb24ec4d54f09a87
podlesniyag <podlesniyag@yandex.ru>
2015-07-07 15:41:23 +0300

    create calculate

--- calculate.java
+++ calculate.java
@@ -1,11 +1,8 @@
-public class calculate
+public class Calculate 
 {
-   public static void main(String[] arg)
-	{
-		System.out.println("calculate...");
-		int first = Integer.valueOf(arg[0]);
-		int second = Integer.valueOf(arg[1]);
-		int summ = first + second;
-		System.out.println("Summ = " + summ);
-	};  
-};
+  public static void main (String[] arg)
+    {
+       System.out.println("Calculate...");
+    }
+
+}
